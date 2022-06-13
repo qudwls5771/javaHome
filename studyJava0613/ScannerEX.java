1458eae094b32b465563acb878475695f4f99d08
qudwls5771 <qudwls5771@naver.com>
2022-06-13 12:29:12 +0900

    Scanner

--- studyJava0613/ScannerEX.java
+++ studyJava0613/ScannerEX.java
@@ -1,24 +1,25 @@
 package studyJava0613;
-import java.util.Scanner;
+import java.util.Scanner; //import는 객체를 불러옴
 
 
-class Main {
+public class ScannerEX {
     
     public static void main(String[] args){
         System.out.println("이름, 도시, 나이, 체중, 독신 여부를 빈칸으로 분리하여 입력하세요.");
-        Scanner scanner = new Scanner(System.in);
+        //new = 새롭게 만들고, 구조를 만든다. , system.in =인자값(객체,통로)
+        Scanner scanner = new Scanner(System.in); 
 
         String name = scanner.next();
         System.out.println("이름은"+name+",");
 
         String city = scanner.next();
-        System.out.println("이름은"+city+",");
+        System.out.println("도시"+city+",");
 
         int age = scanner.nextInt();
-        System.out.println("이름은"+age+"살,");
+        System.out.println(age+"살,");
 
         double weight = scanner.nextDouble();
-        System.out.println("이름은"+weight+"Kg,");
+        System.out.println("체중"+weight+"Kg,");
 
         boolean isSingle = scanner.nextBoolean();
         System.out.println("독신여부"+isSingle+"입니다.");
