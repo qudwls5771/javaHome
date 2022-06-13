948aa25c18f451c1668ffffed4e15186d17e2277
qudwls5771 <qudwls5771@naver.com>
2022-06-13 11:36:38 +0900

    java

---
+++ studyJava0613/Scanner.java
@@ -0,0 +1,16 @@
+package studyJava0613;
+import java.util.Scanner;
+
+
+public class Scanner {
+    
+    public static void main(String[] args){
+        System.out.println("이름, 도시, 나이, 체충, 독신 여부를 빈칸으로 분리하여 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+    
+}
