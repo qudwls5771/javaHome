e247d4783d92d011f38162f97ab6c91d93ca4e2d
qudwls5771 <qudwls5771@naver.com>
2022-06-13 12:00:24 +0900

    Scanner

--- studyJava0613/Scanner.java
+++ studyJava0613/Scanner.java
@@ -2,13 +2,26 @@ package studyJava0613;
 import java.util.Scanner;
 
 
-public class Scanner {
+class Main {
     
     public static void main(String[] args){
-        System.out.println("이름, 도시, 나이, 체충, 독신 여부를 빈칸으로 분리하여 입력하세요.");
+        System.out.println("이름, 도시, 나이, 체중, 독신 여부를 빈칸으로 분리하여 입력하세요.");
         Scanner scanner = new Scanner(System.in);
 
+        String name = scanner.next();
+        System.out.println("이름은"+name+",");
 
+        String city = scanner.next();
+        System.out.println("이름은"+city+",");
+
+        int age = scanner.nextInt();
+        System.out.println("이름은"+age+"살,");
+
+        double weight = scanner.nextDouble();
+        System.out.println("이름은"+weight+"Kg,");
+
+        boolean isSingle = scanner.nextBoolean();
+        System.out.println("독신여부"+isSingle+"입니다.");
 
 
     }
