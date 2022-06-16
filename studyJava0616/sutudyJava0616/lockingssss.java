76e03d7987de01538e18780ced4930e08f88db99
qudwls5771 <qudwls5771@naver.com>
2022-06-16 16:06:12 +0900

    자물쇠, 열쇠_프로그래머스

--- studyJava0616/sutudyJava0616/lockingssss.java
+++ studyJava0616/sutudyJava0616/lockingssss.java
@@ -13,29 +13,43 @@ public class lockingssss {
 
     }
 
-    private static boolean solution(int[][] key, int[][] lock) {
+    public static boolean solution(int[][] key, int[][] lock) {
         boolean answer = true;
         int keyX = key.length;
         int lockY = lock.length;
-        int[][] locking = new int[lockY*3][lockY*3];
+        int[][] locking = new int[lockY*3][lockY*3];  //영향을 안준다. 
 
-            //자물쇠 3x3으로 만든다.
+            //자물쇠 3x3으로 만든다. , 크키를 2배를 줘서 
             for(int i = 0; i<lockY; i++){
                 for(int j=0; j<lockY; j++){
                     locking[lockY+i][lockY+j] = lock[i][j];
                 }
             }
-
-            for(int j =0; j<locking.length; j++){
-                System.out.println(Arrays.toString(locking));
+            for(int j =0; j<lock.length; j++){
+                System.out.println("lock"+Arrays.deepToString(locking));
             }
-            
+
+            //자물쇠 위치를 찾는다? 맞나?
+            for(int i =0; i<lockY; i++){
+                for(int j =0; j<lockY; j++){
+
+                }
+            }
+
 
             //key값에 = typoon(key)메소드를 넣는다.
             for(int k = 0; k<4; k++){
                 key = typoon(key);
             }
-            System.out.println(key);
+            System.out.println("키 값:"+key);
+
+            
+
+
+
+
+
+
 
 
         return answer;
@@ -53,4 +67,19 @@ public class lockingssss {
                     }
                     return tone;
                 }
+
+            //자물쇠로 체크한다.
+            public static boolean ok(int[][] locking){
+                int count = 0;
+                int hole = locking.length/3;
+                for(int i = hole; i<hole*3; i++){
+                    for(int j =hole; j<hole*3; j++){
+                        if(locking[i][j] == 1){ //hole은 안됨 숫자만 가능 1
+                            count++; 
+                        }
+                    }
+                    System.out.println("홀값 :"+hole);
+                }
+                return count == hole;  
+            }
 }
