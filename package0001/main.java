b8c50291af4a2c7a17b0e846d6ad0480005f211b
qudwls5771 <qudwls5771@naver.com>
2022-06-17 15:54:34 +0900

    객체의 이해

--- package0001/main.java
+++ package0001/main.java
@@ -7,16 +7,13 @@ public class main {
         // 스택    , 힙
         Car myCar = new Car();
 
-
         // 인스턴스 내용 출력
         System.out.println(" : " + myCar.company);
-
 
         // 인스턴스의 내용 출력
         // myCar에서 스피드는 0으로 초기값이 되어 있었지만 main에 speed에 대한 필드 데이터를 부여
         myCar.speed = 60;
         System.out.println("myCar : " + myCar.speed);
-
 
         int check = myCar.speedCheck();
         System.out.println("this_speed : "+ check);
