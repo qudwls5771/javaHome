c6c34a2192ae8b57c801337b4302088b958729e7
qudwls5771 <qudwls5771@naver.com>
2022-06-18 21:15:24 +0900

    서버, 클라이언트 연결

--- package0001/Server.java
+++ package0001/Server.java
@@ -1,15 +1,15 @@
 import java.net.ServerSocket;
 
 import java.io.IOException;
-import java.net.ServerSocket;;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         try {
             int socket = 1234;
-            ServerSocket severSocket = new ServerSocket("소켓 포트 만들어야 댐!");
+            ServerSocket severSocket = new ServerSocket(socket);
             System.out.println("Socket : " + socket + "으로 서버가 열렸습다.");
 
 
@@ -18,7 +18,7 @@ public class Server {
                 // 소켓 서버로 접속시 socketUser에 접속자 정보 할당
                 Socket socketUser = severSocket.accept(); 
                 //접속자 getLocalAddress를 가져온다.
-                System.out.println("Client가 접속함 : " socketUser.getLocalAddress());
+                System.out.println("Client가 접속함 : " + socketUser.getLocalAddress());
 
 
             }
--- package0001/socket.java
+++ package0001/socket.java
@@ -5,7 +5,7 @@ public class socket {
 
     public static void main(String[] args){
         try {
-			Socket socket = new Socket("121.999.999.99", 1234); // 소켓 서버에 접속 
+			Socket socket = new Socket("112.157.248.200", 1234); // 소켓 서버에 접속 
 			System.out.println("socket 서버에 접속 성공!");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
