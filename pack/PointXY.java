80e7e7215c51c024bea29645db4d24eb81610440
qudwls5771 <qudwls5771@naver.com>
2022-06-19 23:26:08 +0900

    x,y축 좌표연습

--- pack/PointXY.java
+++ pack/PointXY.java
@@ -14,9 +14,22 @@ class PointXY{
     }
 
     public void setY(int y){
-        this.x =x;
+        this.y =y;
     }
 
+    //증가시킬 x,y값 입력받아 멤버변수에 x,y값 증가시켜 담아두기
+    public void moveXY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
 
 
 }
---
+++ pack/main;.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args){
+        //실행1
+        PointXY point = new PointXY(100, 100); 
+
+        //실행2
+        point.setX(200);
+
+        //실행3
+        point.setY(300);
+
+        //실행4
+        point.moveXY(50,50);
+
+        System.out.println(getX() + " " + getY()) ;
+
+
+    }
+    
+}
