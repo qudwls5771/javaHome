bd09cc433e05dcff6700315e47fb55ed201384be
qudwls5771 <qudwls5771@naver.com>
2022-06-19 23:21:28 +0900

    x축 y축 연습

--- pack/PointXY.java
+++ pack/PointXY.java
@@ -3,10 +3,20 @@
 class PointXY{
     private int x;
     private int y;
-
+    //객체 생성 : x,y 입력 받는 형태의 생성자 정의 
     PointXY(int x, int y){
-        
+        this.x = x;
+        this.y  =y;
     }
+    //x,y값 입력받아 멤버변수 x에 담아뒤기
+    public void setX(int x){
+        this.x =x;
+    }
+
+    public void setY(int y){
+        this.x =x;
+    }
+
 
 
 }
