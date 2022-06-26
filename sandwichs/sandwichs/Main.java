b333bdbf14d240398c56e1ffe435defa162e6a65
qudwls5771 <qudwls5771@naver.com>
2022-06-26 22:20:59 +0900

    랜덤

--- sandwichs/sandwichs/Main.java
+++ sandwichs/sandwichs/Main.java
@@ -1,49 +1,35 @@
 package sandwichs;
-import java.util.*;
-public class Main {
-    public static void main(String[] arg){
 
- 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] arg) {
+
         ArrayList<mo> moList = new ArrayList<mo>();
 
-        for (int i = 0; i < itemList.size(); i++) {
-            item item_T = itemList.get(i);
-            item_T.showItemInfo();
-            itemList.get(((int)(Math.random() * (item_T.size()-1))));
-        }
-        System.out.println();
-        
-
-        
-        
-        
-        
-        for(int i=0 ;i < moList.size(); i++){
+        for (int i = 0; i < moList.size(); i++) {
             mo moLists = moList.get(i);
-            moList.get(((int)(Math.random() * (moList.size()-1))));
-            if(moLists)
-            
-      }
+            moLists.showItemInfo();
+            moList.get(((int) (Math.random() * (moList.size() - 1))));
+
+            if (i == 0) {
+                System.out.println("월요일 샌드위치입니다.");
+            }
+            if (i == 1) {
+                System.out.println("화요일 샌드위치입니다.");
+            }
+            if (i == 2) {
+                System.out.println("수요일 샌드위치입니다.");
+            }
+            if (i == 3) {
+                System.out.println("목요일 샌드위치입니다.");
+            }
+            if (i == 4) {
+                System.out.println("금요일 샌드위치입니다.");
+            }
+
+        }
 
     }
 
-
-/*     if(i == 0) {
-        System.out.println("월요일 샌드위치입니다.");
-    }
-    if(i == 1){
-        System.out.println("화요일 샌드위치입니다.");
-    }
-    if(i == 2){
-        System.out.println("수요일 샌드위치입니다.");
-    }
-    if(i == 3){
-        System.out.println("목요일 샌드위치입니다.");
-     }
-    if(i == 4){
-        System.out.println("금요일 샌드위치입니다."); 
-    } */
-
-    
-    
 }
