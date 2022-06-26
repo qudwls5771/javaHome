46e27525a031a8df452e1918698051a1a5199256
qudwls5771 <qudwls5771@naver.com>
2022-06-27 00:44:08 +0900

    과제

--- sandwich/sandwich/sandwich/Main.java
+++ sandwich/sandwich/sandwich/Main.java
@@ -3,14 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg){
         
-        
-     
-        
-       
-        
-        pumkinsand pumkinsand = new pumkinsand("빵", "버터", "호박");
-        pumkinsand.showInfo();
-
 
         ArrayList<pumkinsand> pri = new ArrayList<>();
         pri.add(new pumkinsand("빵", "버터", "호박"));
