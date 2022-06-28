0a9483fcbd2a66dae8701fac4a22d750b5f0d387
qudwls5771 <qudwls5771@naver.com>
2022-06-28 16:51:53 +0900

    도형

---
+++ studyJava0628/도형.java
@@ -0,0 +1,98 @@
+public class 도형 {
+    private static void startForIf(){
+        for(int i = 0; i< 6; i++){
+            for(int j = 0; j<6; j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+
+
+    }
+
+    private static void startForIs(){
+        for(int i =0; i< 6; i++){
+            for(int j = 0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+
+    private static void startForIss(){
+
+        for(int i =0; i< 6; i++){
+            for(int j = i; j<6; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+
+
+    private static void startForIsss(){
+        for(int i = 1; i< 5; i++){
+            for(int j =1; j<5-i; j++ ){
+                System.out.print(" ");
+            }
+            for(int k = 0; k< i*2-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = 4; i> 0; i--){
+            for(int j =1; j<5-i; j++ ){
+                System.out.print(" ");
+            }
+            for(int j = 0; j< i*2-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    private static void startForIff(){
+        for(int i = 1; i<5; i++){
+            for(int j =1; j<5-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k< i*2-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = 4; i> 0; i--){
+            for(int j =1; j<5-i; j++ ){
+                System.out.print(" ");
+            }
+            for(int j = 0; j< i*2-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void main(String[] args){
+     //   hero Hero = new hero();
+     //   Hero.attack();
+
+
+        startForIf(); //직사각형
+        startForIs(); //직각삼각형
+        startForIss();  //역삼각형
+        startForIsss(); //마름모
+        //startForIff(); 
+    }
+    
+}
