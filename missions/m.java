f07f44c018c205bfa097d53f8179200c31852f80
qudwls5771 <qudwls5771@naver.com>
2022-07-17 07:13:22 +0900

    시험

--- missions/m.java
+++ missions/m.java
@@ -3,13 +3,13 @@ public class m {
         int a = 1, b = 2, c = 3, d = 4;
         int mx, mn;
         mx = a<b? b: a;
-        System.out.println(mx);
+        System.out.println("mx값"+mx);
         if(mx==1){
             mn = a > mx ? b : a;
             System.out.println(mn);
         }else {
             mn = b < mx ? d : c;
         }
-        System.out.println(mn);
+        System.out.println("정답"+mn);
     }
 }
