98b58c7178378ac24ebc2fca44cf7688912fd811
qudwls5771 <qudwls5771@naver.com>
2022-07-24 23:11:41 +0900

    쇼핑몰

--- Team_Pro/ShopDAO.java
+++ Team_Pro/ShopDAO.java
@@ -1,7 +1,12 @@
 public class ShopDAO {
+
+    List<ShopDTO> shopDTO = new ArrayList<ShopDTO>();
+
     private ShopDAO(){}
     private static ShopDAO instance = new ShopDAO();
     public static ShopDAO getInstancce(){
         return instance;
     }
+
+    
 }
