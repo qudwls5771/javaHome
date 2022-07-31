a011d8c876a7be12e875890702ab3c17b93ad516
qudwls5771 <qudwls5771@naver.com>
2022-07-31 22:37:49 +0900

    애견동반호텔

--- Team_Pro/DTO/HotelDTO.java
+++ Team_Pro/DTO/HotelDTO.java
@@ -4,7 +4,37 @@ public class HotelDTO {
     private String address; // 호텔주소
     private Integer price; //가격
     private String picture; // 가게사진
-
-
+    public Integer getCode_H() {
+        return code_H;
+    }
+    public void setCode_H(Integer code_H) {
+        this.code_H = code_H;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    public String getPicture() {
+        return picture;
+    }
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+    
+    
     
 }
