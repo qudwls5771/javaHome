f44c4f1c7c4c6d9abe93d03f11e4657c523169df
qudwls5771 <qudwls5771@naver.com>
2022-07-31 22:36:11 +0900

    애견동반호텔

--- Team_Pro/DTO/HotelDTO.java
+++ Team_Pro/DTO/HotelDTO.java
@@ -1,6 +1,10 @@
 public class HotelDTO {
-    private  Integer code_S; // 호텔 코드번호
+    private  Integer code_H; //호텔 코드번호
     private String name; //이름
+    private String address; // 호텔주소
+    private Integer price; //가격
+    private String picture; // 가게사진
+
 
     
 }
